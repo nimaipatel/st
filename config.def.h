3cc0d69460dd9072dd8dc846ac9e746718897613
nimai <nimai.m.patel@gmail.com>
2021-03-26 17:14:18 +0530

    scroll without holding shiftdown

--- config.def.h
+++ config.def.h
@@ -179,11 +179,11 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+	{ XK_ANY_MOD,            Button4, kscrollup,      {.i = 1} },
+	{ XK_ANY_MOD,            Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
+	{ XK_ANY_MOD,            Button4, ttysend,        {.s = "\033[5;2~"} },
+	{ XK_ANY_MOD,            Button5, ttysend,        {.s = "\033[6;2~"} },
 };
 
 /* Internal keyboard shortcuts. */
