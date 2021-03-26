bec8264613d5fba8d359c84d646493268642fe99
nimai <nimai.m.patel@gmail.com>
2021-03-26 17:05:41 +0530

    font 2 patch + font configuration

--- config.def.h
+++ config.def.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Fixed:pixelsize=18:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
