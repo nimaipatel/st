36958a3b7a0e7d8fbcc9cbbc1caaae5583b63ea3
nimai <nimai.m.patel@gmail.com>
2021-04-09 14:38:10 +0530

    use monospace at compile time, and let xresources handle font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Fixed:pixelsize=18:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
