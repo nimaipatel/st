b58b96bb1a27cdce4402bcecbd9787c21fc320b8
nimai <nimai.m.patel@gmail.com>
2021-04-22 21:04:44 +0530

    fallback with NF just in case

--- config.def.h
+++ config.def.h
@@ -9,6 +9,7 @@ static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+	"Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
