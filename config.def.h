07b291e522217b92ec675f31d00a27ff53f16181
nimai <nimai.m.patel@gmail.com>
2021-05-06 16:49:45 +0530

    reduced default opacity

--- config.def.h
+++ config.def.h
@@ -100,7 +100,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.90, alphaUnfocused = 0.80;
+float alpha = 0.80, alphaUnfocused = 0.70;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
