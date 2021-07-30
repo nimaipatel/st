d250291be971e9dfeeef212e1c80c30cca4c3a2d
nimai <nimai.m.patel@gmail.com>
2021-07-30 18:37:42 +0530

    Fix fonts

--- config.def.h
+++ config.def.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
-	"Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
+	"Symbols Nerd Font:pixelsize=12:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
